f037832b0575b1b4837748942a157a35910c6f96
saltduke <greimlch2@gmail.com>
2025-03-26 16:02:05 +0100

    add stl files to repo for split meshes and create class for peg placement

--- franka_sim/__init__.py
+++ franka_sim/__init__.py
@@ -19,3 +19,9 @@ register(
     max_episode_steps=100,
     kwargs={"image_obs": True},
 )
+register(
+    id="PandaInsertPegVision-v0",
+    entry_point="franka_sim.envs:PandaInsertPegGymEnv",
+    max_episode_steps=100,
+    kwargs={"image_obs": True},
+)
